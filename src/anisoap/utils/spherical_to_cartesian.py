bcf2e84fb4072ed8c3df1fa50ec323e3a3cea398
Kevin Huguenin <kamehugucode@gmail.com>
2022-12-01 19:08:20 +0100

    Add spherical to cartesian test
    Add spherical to cartesian transformer
    
    Fix spherical to Cartesian tf. bugs, all tests pass

--- src/anisoap/utils/spherical_to_cartesian.py
+++ src/anisoap/utils/spherical_to_cartesian.py
@@ -1,18 +1,87 @@
 import numpy as np
+import scipy
+from scipy.special import factorial, factorial2
+from math import comb
+from anisoap.utils import monomial_iterator
 
-def spherical_to_cartesian(lmax, nmaxs):
-    assert len(nmaxs) == lmax
+def get_P_ll(l,x):
+    return factorial2(2*l-1)*(-1)**l* np.sqrt(1-x**2)**l
+
+#We are implementing iterations of the form R_{l+1} = prefact_minus0* z * R_{l} + prefact_minus1* r2 * R_{l-1}
+def prefact_minus0(l):
+    m=np.arange(-l,l+1)
+    return np.sqrt(factorial(l+1-m)/factorial(l+1+m)) * np.sqrt(factorial(l+m)/factorial(l-m)) * (2*l+1)/(l+1-m)
+    
+def prefact_minus1(l):
+    m=np.arange(-l+1,l)
+    return -1* np.sqrt(factorial(l+1-m)/factorial(l+1+m)) * np.sqrt(factorial(l-1+m)/factorial(l-1-m)) * (l+m)/(l+1-m)
+
+def binom(n, k):
+    return comb(n, k)
+#     return math.factorial(n) / (math.factorial(k) * math.factorial(n - k))
+
+
+def spherical_to_cartesian(lmax, num_ns):
+    assert len(num_ns) == lmax + 1
+
+    # Initialize array in which to store all
+    # coefficients for each l
+    # Usage T_l[m,n,n0,n1,n2]
     T = []
-    for l, nmax in enumerate(nmaxs):
-        # Initialize array in which to store all
-        # # coefficients for each l
-        maxdeg = l + 2*nmax
-
-        # Usage T_l[m,n,n0,n1,n2]
-        T_l = np.zeros((2*l+1,nmax,maxdeg+1,maxdeg+1,maxdeg+1))
-        
-        # TODO: Compute coefficients
-
+    for l, num_n in enumerate(num_ns):
+        maxdeg = l + 2*(num_n-1)
+        T_l = np.zeros((2*l+1,num_n,maxdeg+1,maxdeg+1,maxdeg+1))
         T.append(T_l)
+
+    # Initialize array in which to store all coefficients for each l
+    # Usage T_l[m,n,n0,n1,n2]
+    T[0][0,0,0,0,0] = 1
+    for l in range(1,lmax+1):
+        prefact = np.sqrt(2) * factorial2(2*l-1) / np.sqrt(factorial(2*l))
+        for k in range(l//2+1):
+            n1 = 2*k
+            n0 = l-n1
+            T[l][2*l,0, n0, n1,0] = binom(l, n1) *(-1)**k  
+        for k in range((l-1)//2+1):
+            n1 = 2*k+1
+            n0 = l-n1
+            T[l][0,0,n0,n1,0] = binom(l, n1) *(-1)**k
+        T[l]*= prefact
+
+    # Run iteration over (l,m) to generate all coefficients for n=0.
+    for l in range(lmax):
+        deg = l
+        myiter = iter(monomial_iterator.TrivariateMonomialIndices(deg+1))
+        for idx,n0,n1,n2 in myiter:
+            a = prefact_minus0(l) # length 2l+1 due to m dependence
+            b = prefact_minus1(l) # length 2l+1 due to m dependence
+    
+            #(-l+1)+2: (l+1) -2 gets contributions from T[l-1]
+            if n0-2>=0:
+                T[l+1][2:2*l+1,0,n0,n1,n2] += b * T[l-1][:,0,n0-2,n1,n2]
+            if n1-2>=0:
+                T[l+1][2:2*l+1,0,n0,n1,n2] += b * T[l-1][:,0,n0,n1-2,n2]
+            if n2-2>=0:
+                T[l+1][2:2*l+1,0,n0,n1,n2] += b * T[l-1][:,0,n0,n1,n2-2]
+            #(-l+1)+1: (l+1) -1 gets contributions from T[l]
+            if n2-1>=0:
+                T[l+1][1:2*l+2,0,n0,n1,n2] += a * T[l][:,0,n0,n1,n2-1]
+
+    # Run the iteration over n
+    for l in range(lmax+1):
+        for n in range(1,num_ns[l]):
+            deg = l + 2*n # degree of polynomial
+            myiter = iter(monomial_iterator.TrivariateMonomialIndices(deg))
+            for idx,n0,n1,n2 in myiter:
+                # Use recurrence relation to update
+                # Warning, if n0-2, n1-2 or n2-2 are negative
+                # it might be necessary to add if statements
+                # to avoid.
+                if n0>=2:
+                    T[l][:,n,n0,n1,n2] += T[l][:,n-1,n0-2,n1,n2]
+                if n1>=2:
+                    T[l][:,n,n0,n1,n2] += T[l][:,n-1,n0,n1-2,n2]
+                if n2>=2:
+                    T[l][:,n,n0,n1,n2] += T[l][:,n-1,n0,n1,n2-2]
 
     return T
---
+++ tests/test_spherical_to_cartesian.py
@@ -0,0 +1,125 @@
+import numpy as np
+from numpy.testing import assert_allclose
+import pytest
+from math import factorial
+from anisoap.utils import TrivariateMonomialIndices, assert_close, spherical_to_cartesian
+
+# Generate the exact values of the spherical to
+# Cartesian coordinate transformations for l=0,1,2.
+# For l=0 and l=1, arbitrary degrees in n are supported, 
+# while for l=2, only n=0 is supported.
+def spherical_to_cartesian_exact(lmax, num_ns):
+    T = []
+    assert lmax <= 3, "Exact analytical expressions only implemented for lmax<=3"
+    assert len(num_ns) == lmax+1, "Provided list of nmax values has to match number of l values"
+
+    # Initialize the arrays in the same way as the main code
+    for l, nmax in enumerate(num_ns):
+        # Initialize array in which to store all
+        # # coefficients for each l
+        maxdeg = l + 2*(nmax-1)
+
+        # Usage T_l[m,n,n0,n1,n2]
+        shape = (2*l+1,nmax,maxdeg+1,maxdeg+1,maxdeg+1)
+        T_l = np.zeros((2*l+1,nmax,maxdeg+1,maxdeg+1,maxdeg+1))
+        T.append(T_l)
+
+    # Fill in the exact coefficients for l=0
+    for n in range(num_ns[0]):
+        monomial_indices = iter(TrivariateMonomialIndices(deg = n))
+        fac_n = factorial(n) 
+        for idx, n0, n1, n2 in monomial_indices:
+            coeff = fac_n/factorial(n0)/factorial(n1)/factorial(n2)
+            T[0][0,n,2*n0,2*n1,2*n2] = coeff
+    if lmax == 0:
+        return T
+
+    # Fill in the exact coefficients for l=1
+    for n in range(num_ns[1]):
+        monomial_indices = iter(TrivariateMonomialIndices(deg = n))
+        fac_n = factorial(n) 
+        for idx, n0, n1, n2 in monomial_indices:
+            coeff = fac_n/factorial(n0)/factorial(n1)/factorial(n2)
+            T[1][0,n,2*n0,2*n1+1,2*n2] = coeff # m=-1 with solid harmonic = y
+            T[1][1,n,2*n0,2*n1,2*n2+1] = coeff # m=0 with solid harmonic = z
+            T[1][2,n,2*n0+1,2*n1,2*n2] = coeff # m=1 with solid harmonic = x
+    if lmax == 1:
+        return T
+    
+    # Fill in the exact coefficients for l=2 and n=0.
+    assert num_ns[2] == 1, "For l=2, only n=0 is implemented"
+    T[2][0,0,1,1,0] = np.sqrt(3) # m=-2
+    T[2][1,0,0,1,1] = np.sqrt(3) # m=-1
+    T[2][3,0,1,0,1] = np.sqrt(3) # m=+1
+    
+    # Coeffs for m=0
+    T[2][2,0,2,0,0] = -0.5
+    T[2][2,0,0,2,0] = -0.5
+    T[2][2,0,0,0,2] = 1
+    
+    # Coeffs for m=+2
+    T[2][4,0,2,0,0] = np.sqrt(3)/2
+    T[2][4,0,0,2,0] = -np.sqrt(3)/2
+        
+    return T
+
+@pytest.mark.parametrize('lmax', np.arange(5))
+@pytest.mark.parametrize('num_n', np.arange(1,5))
+def test_compare_against_exact(lmax, num_n):
+    assert num_n >= 1, "Number of radial channels has to be at least 1"
+
+    # Compute the spherical to Cartesian transformation coefficients
+    # using the calculator in the main code as well as the reference
+    # code using analytical expressions that was defined above.
+    T_ref = spherical_to_cartesian_exact(lmax=2, num_ns=[num_n,num_n,1]) # reference values 
+    T = spherical_to_cartesian(lmax, num_ns=[num_n]*(lmax+1))
+
+    # Compare the obtained coefficients against the exact values
+    assert_allclose(T[0], T_ref[0], rtol=1e-14)
+    if lmax >= 1:
+        assert_allclose(T[1], T_ref[1], rtol=1e-14)
+    if lmax >= 2:
+        assert_allclose(T[2][:,0,:3,:3,:3], T_ref[2][:,0], rtol=1e-14)
+
+
+class TestSumOfCoefficients:
+    """
+    While it is not as simple to generate all transformation coefficients
+    from scratch using an alternative method, it is much simpler to check
+    that the sum of all coefficients is equal to the correct value.
+    This is being tested in this class.
+    """
+    lmaxs = np.arange(8)
+    num_ns = np.arange(1,14)
+    # For l=0 and arbitrary n, the sum of all coefficients
+    # has to equal 3^n.
+    # This follows from the fact that the solid harmonics at
+    # (l,m)=(0,0) is just equal to the constant 1 function,
+    # and thus the coefficients are simply obtained from
+    # the expansion of (x^2+y^2+z^2)^n.
+    # The sum of all such coefficients is simply equal to
+    # the special case of the above for x=y=z=1, meaning
+    # that the sum of the coefficients will be equal to 3^n.
+    @pytest.mark.parametrize('num_n', num_ns)
+    def test_sum_for_l0(self, num_n):
+        assert num_n >= 1
+        lmax = 0
+        T = spherical_to_cartesian(lmax, num_ns=[num_n])
+        for n in range(num_n):
+            T_0n = T[0][0,n]
+            sum_of_all = np.sum(T_0n)
+            assert_close(sum_of_all, 3.**n, rtol=1e-14)
+
+    # We now repeat the same test also for l=1,2,3.
+    # The sum of coefficients for some n is always 3^n times
+    # the sum of coefficients at n=0 which has been computed
+    # explicity.
+    @pytest.mark.parametrize('lmax', lmaxs) 
+    @pytest.mark.parametrize('num_n', num_ns)
+    def test_sum_of_coefficients(self, lmax, num_n):
+        assert num_n >= 1
+        T = spherical_to_cartesian(lmax, num_ns=[1]*(lmax)+[num_n])
+        ref_value = np.sum(T[lmax][:,0]) 
+        for n in range(num_n):
+            sum_of_all = np.sum(T[lmax][:,n])
+            assert_close(sum_of_all, ref_value*(3.0**n), rtol=1e-14)
