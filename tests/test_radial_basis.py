f1754b66f601fea33ff3ceb2b75bc201f3649e5c
Arthur Lin <alin62@wisc.edu>
2023-06-28 20:27:17 -0500

    added tests to test gto utils

--- tests/test_radial_basis.py
+++ tests/test_radial_basis.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytest
 from numpy.testing import assert_allclose
+from sklearn.datasets import make_spd_matrix
 from scipy.spatial.transform import Rotation
 
 # internal imports
@@ -108,3 +109,81 @@ class TestGaussianParameters:
         atol = 1e-15 / sigma**2  # largest elements of matrix will be 1/sigma^2
         assert_allclose(center_gto, center_ref, rtol=1e-10, atol=atol)
         assert_allclose(prec_gto, prec_ref, rtol=1e-10, atol=atol)
+
+class TestGTOUtils:
+
+    # Create a list of semipositive definite matrices (spd), seminegative definite matrices (snd), and
+    # nonsymmetric matrices for testing
+    spd_matrices = []
+    snd_matrices = []
+    nonsym_matrices = []
+    for _ in range(100):
+        dim = np.random.randint(2, 100)
+        spd = make_spd_matrix(dim)
+        spd_matrices.append(spd)
+        snd_matrices.append(-spd)
+        nonsym_matrices.append(np.random.random(size=(dim, dim)))
+
+    num_trials = 100
+    basis_sizes = np.random.randint(2, 15, num_trials)
+
+    @pytest.mark.parametrize("spd", spd_matrices)
+    def test_spd_inverse_sqrt_no_exceptions(self, spd):
+        # Assert that exception is never raised for semipositive definite matrices
+        try:
+            radial_basis.inverse_matrix_sqrt(spd)
+        except ValueError:
+            assert False, f"calling inverse matrix square root on {spd} raised a value error"
+
+    @pytest.mark.parametrize("snd", snd_matrices)
+    def test_npd_inverse_sqrt_all_exceptions(self, snd):
+        # Assert that exception is ALWAYS raised for seminegative definite matrices
+        with pytest.raises(ValueError):
+            radial_basis.inverse_matrix_sqrt(snd)
+
+    @pytest.mark.parametrize("nonsym", nonsym_matrices)
+    def test_nonsymmetric_inverse_sqrt_all_exceptions(self, nonsym):
+        # Assert that exception is ALWAYS raised for nonsymmetric matrices
+        with pytest.raises(ValueError):
+            radial_basis.inverse_matrix_sqrt(nonsym)
+
+    @pytest.mark.parametrize("spd", spd_matrices)
+    def test_spd_inverse_sqrt(self, spd):
+        dim = np.shape(spd)[0]
+        inv_sqrt_s = radial_basis.inverse_matrix_sqrt(spd)
+        assert_allclose(np.eye(dim), inv_sqrt_s @ inv_sqrt_s @ spd, rtol=1e-9, atol=1e-9)
+
+    @pytest.mark.parametrize("basis_size", basis_sizes)
+    def test_orthonormality(self, basis_size):
+        # Create an array of GTO order n and GTO width sigma for testing orthonormality of basis set.
+        # Set upper limit of n to be 20, sigma to be 3
+        n_grid = np.random.randint(1, 4, basis_size)
+        sigma_grid = np.random.uniform(0.1, 3, basis_size)
+        # mesh to evaluate GTOs
+        n_points = 100000
+        r_mesh = np.linspace(0, 100, n_points)
+        gto = np.zeros((basis_size, n_points))
+        for i in range(basis_size):
+            n = n_grid[i]
+            sigma = sigma_grid[i]
+            gto_n_sigma = r_mesh ** n * np.exp(-r_mesh ** 2 / (2 * sigma ** 2))
+            gto[i, :] = gto_n_sigma
+
+        S = radial_basis.gto_overlap(
+            n_grid[:, np.newaxis],
+            n_grid[np.newaxis, :],
+            sigma_grid[:, np.newaxis],
+            sigma_grid[np.newaxis, :],
+        )
+        gto_orthonorm = radial_basis.inverse_matrix_sqrt(S) @ gto
+
+        # Evaluate the overlap between orthonormal GTOs using trapezoidal integration.
+        # This is integration scheme is pretty numerically bad it's just used to prove a point.
+        # Hence, we use a pretty large absolute tolerance.
+        overlaps = np.zeros((basis_size, basis_size))
+        for i in range(basis_size):
+            for j in range(basis_size):
+                overlap = np.trapz(gto_orthonorm[i, :] * gto_orthonorm[j, :] * r_mesh**2, r_mesh)
+                overlaps[i][j] = overlap
+
+        assert_allclose(np.eye(basis_size), overlaps, rtol=0, atol=1e-3)
