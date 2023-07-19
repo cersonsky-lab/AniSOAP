1e3f60f905d18e61aceca481608e7d3780b9e281
Patron <Patron@Patrons-Air.attlocal.net>
2023-07-19 15:01:07 -0700

    Raise an error when a float is passed for radial_gaussian_width, and added a unit test to ensure error is raised

--- tests/test_ellipsoidal_density_projection.py
+++ tests/test_ellipsoidal_density_projection.py
@@ -129,6 +129,11 @@ class TestBadInputs:
             ValueError,
             "We have only implemented transforming quaternions (`quaternion`) and rotation matrices (`matrix`).",
         ],
+        [
+            {**DEFAULT_HYPERS, "radial_gaussian_width": 9},
+            ValueError,
+            "radial_gaussian_width is set as an integer, which could cause overflow errors. Pass in float."
+        ]
     ]
 
     @pytest.mark.parametrize("hypers,error_type,expected_message", test_hypers)
