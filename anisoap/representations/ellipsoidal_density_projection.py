86c94294b7c640b4ce5dfb4e0c3498fba9d661e7
Rose K. Cersonsky <47536110+rosecers@users.noreply.github.com>
2024-03-05 11:59:11 -0600

    Adding gaussian normalization

--- anisoap/representations/ellipsoidal_density_projection.py
+++ anisoap/representations/ellipsoidal_density_projection.py
@@ -126,11 +126,18 @@ def pairwise_ellip_expansion(
 
                     rot = rotation_matrices[j_global]
                     lengths = ellipsoid_lengths[j_global]
-                    precision, center = radial_basis.compute_gaussian_parameters(
-                        r_ij, lengths, rot
-                    )
+                    volume = 4.0 / 3.0 * np.pi * np.product(lengths / 2.0)
 
-                    moments = compute_moments_inefficient_implementation(
+                    length_norm = (
+                        np.product(lengths) * volume * (2.0 * np.pi) ** (3.0 / 2.0)
+                    ) ** -1.0
+
+                    (
+                        precision,
+                        center,
+                    ) = radial_basis.compute_gaussian_parameters(r_ij, lengths, rot)
+
+                    moments = length_norm * compute_moments_inefficient_implementation(
                         precision, center, maxdeg=maxdeg
                     )
                     for l in range(lmax + 1):
