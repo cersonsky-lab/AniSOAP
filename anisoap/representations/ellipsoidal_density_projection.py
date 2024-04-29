corrected the testing code. rtol should not be np.inf otherwise the test never fails. Also moved the scaling outside of the inner loop, significantly speeds up the iterations

anisoap/representations/ellipsoidal_density_projection.py
@@ -83,6 +83,9 @@ def pairwise_ellip_expansion(
     # This is needed because spherical_to_cartesian calculates solid harmonics Rlm = sqrt((4pi)/(2l+1)) * r^l*Ylm
     # Our expansion coefficients from the inner product does not have this prefactor included, so we divide it later.
     solid_harm_prefact = np.sqrt((4 * np.pi) / (np.arange(lmax + 1) * 2 + 1))
+    scaled_sph_to_cart = []
+    for l in range(lmax + 1):
+        scaled_sph_to_cart.append(sph_to_cart[l] / solid_harm_prefact[l])
     for center_types in types:
         for neighbor_types in types:
             if (center_types, neighbor_types) in neighbor_list.keys:
@@ -135,7 +138,7 @@ def pairwise_ellip_expansion(
                         values_ldict[l].append(
                             np.einsum(
                                 "mnpqr, pqr->mn",
-                                sph_to_cart[l] / solid_harm_prefact[l],
+                                scaled_sph_to_cart[l],
                                 moments_l,
                             )
                         )

tests/test_convergence.py
@@ -96,6 +96,5 @@ class TestGaussianConvergence:
         # we should get a better and better approximation.
         assert np.all(errs[:-1] >= errs[1:])
 
-        # as long as each element differs by a small absolute amount, we're good.
-        # rtol defaults for allclose are hard to satisfy since tails of gaussian approach 0, so we set it to inf.
-        assert_allclose(approx, actual, atol=1e-03, rtol=np.inf)
+        # as long as each element differs by a (somewhat) small absolute amount, we're good.
+        assert_allclose(approx, actual, atol=1e-03)